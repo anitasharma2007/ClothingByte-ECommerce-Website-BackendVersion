7cbdf85a9e32ad3e605ada5b5b1ac38095a2e040
anita <1995.anitasharma@gmail.com>
2025-03-27 21:35:57 +0530

    first commit

---
+++ src/main/java/com/ecommerce/service/AddressService.java
@@ -0,0 +1,8 @@
+package com.ecommerce.service;
+
+import com.ecommerce.entities.Address;
+
+public interface AddressService {
+	Address saveAddress(Address address);
+	Address findAddress(Long id);
+}
---
+++ src/main/java/com/ecommerce/service/PaymentService.java
@@ -0,0 +1,8 @@
+package com.ecommerce.service;
+
+import com.ecommerce.entities.Payment;
+
+public interface PaymentService {
+	Payment savePayment(Payment payment);
+	Payment findPaymentById(Long id);
+}
